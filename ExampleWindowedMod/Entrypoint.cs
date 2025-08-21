9104cdda626fb7682121369d981c2f9daff55fc9
Lexi <arallen3660@gmail.com>
2025-08-21 09:44:49 -0400

    Add in a custom window example

---
+++ ExampleWindowedMod/Entrypoint.cs
@@ -0,0 +1,13 @@
+﻿using Brutal;
+using KSA;
+using KsaLoader.Core;
+
+namespace ExampleWindowedMod;
+
+public class Entrypoint : CodeMod
+{
+    public Entrypoint(Mod definingMod) : base(definingMod)
+    {
+        var window = new Window(new float2(100, 100));
+    }
+}
---
+++ ExampleWindowedMod/Window.cs
@@ -0,0 +1,20 @@
+﻿using Brutal;
+using Brutal.ImGuiApi;
+using KSA;
+using ImGuiWindow = KSA.ImGuiWindow;
+
+namespace ExampleWindowedMod;
+
+public class Window : ImGuiWindow, IStaticWindow
+{
+    public Window(float2 initialSize, bool lockAspectRatio = false) : base(initialSize, lockAspectRatio)
+    {
+        SetWindowTitle("Hello world window!");
+    }
+
+    public override void DrawContent(Viewport viewport)
+    {
+        ImGui.Text("Hello World!");
+    }
+
+}
